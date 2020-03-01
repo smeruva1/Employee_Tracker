20715ae468efb1d683ffbee1c1f8b3ae551311df
Sudar Meruva <meruva_s@yaho.com>
2020-02-29 23:47:52 -0500

    Enhanced code to GET, POST, DELETE, UPDATE Employee Table

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ require('console.table');
 // import connection
 const connection = require('./config/connection');
 
+// const emitter = new EventEmitter()
+// const emitter = require('EventEmitter');
+// emitter.setMaxListeners(100);
+
 // import functions to work with database
 // const { getAllEmployees, createEmployee, updateEmployee } = require('./lib/db-employees');
-const { getAllEmployees, createEmployee, deleteEmployee } = require('./lib/db-employees');
+const { getAllEmployees, createEmployee, deleteEmployee, updateEmployeeRole, updateEmployeeManager } = require('./lib/db-employees');
 
 // import arrays of questions for inquirer prompts
- const { startQuestions, createEmployeeQuestions, deleteEmployeeQuestions
-//, bidQuestions
- } = require('./lib/prompts');
+const { startQuestions, createEmployeeQuestions, deleteEmployeeQuestions, updateEmployeeQuestions } = require('./lib/prompts');
 
 //const startQuestions = require('./lib/prompts');
 
@@ -19,21 +21,22 @@ const { getAllEmployees, createEmployee, deleteEmployee } = require('./lib/db-em
 const startApp = async () => {
     // destructure response object out of first prompt, using await means no .then() needed
     const userAction = await inquirer.prompt(startQuestions);
-    console.log(userAction.EmployeeAction);
+    //console.log(userAction.EmployeeAction);
 
     // depending on the answer, do an action
     //choices: ['Review all Employees', 'Create a new Employee', 'Update an Employee', 'Delete an Employee', 'Exit']
     if (userAction.EmployeeAction === 'Review all Employees') {
-       
+
         getAllEmps();
     } else if (userAction.EmployeeAction === 'Create a new Employee') {
-        console.log("inside question");
         postNewEmp();
     } else if (userAction.EmployeeAction === 'Update an Employee') {
+        //console.log("inside upd question");
         updateEmp();
     } else if (userAction.EmployeeAction === 'Delete an Employee') {
         deleteEmp();
-    } else {
+    } else if (userAction.EmployeeAction === 'Exit') {
+        // console.log("inside exit");
         connection.end();
     }
 };
@@ -48,7 +51,6 @@ const getAllEmps = async () => {
     // print all of the items
     console.table(employees);
 
-
     return startApp();
 };
 
@@ -56,7 +58,7 @@ const getAllEmps = async () => {
 
 // function to create a new employee, defined to be async
 const postNewEmp = async () => {
-    console.log("inside postNewEmp");
+    // console.log("inside postNewEmp");
     // get answers out of inquirer prompt
     const { first_name, last_name, role_id, manager_id } = await inquirer.prompt(createEmployeeQuestions);
 
@@ -71,60 +73,51 @@ const postNewEmp = async () => {
 // function to delete an employee
 const deleteEmp = async () => {
 
-    console.log("inside deleteEmp");
+    // console.log("inside deleteEmp");
     // get all employees so user can see what's there
-    const employees = await getAllEmps();
+    //console.log("inside getallemps");
+    const employees = await getAllEmployees();
 
-    // print all of the employees
+    // print all of the items
     console.table(employees);
 
     // enter employee id to be deleted
     const { id } = await inquirer.prompt(deleteEmployeeQuestions);
 
-    console.log("This id will be deleted --------", id);
+    // console.log("This id will be deleted: ", id);
 
-    deleteEmployee(id);
+    await deleteEmployee(id);
 
-    // // get item we want to bid on based on id
-    // const { id, highest_bid } = items.find(({ id }) => id === parseInt(item_id));
-
-    // // if the current high bid is higher than the users, restart auction
-    // if (highest_bid > bid_amount) {
-    //     console.log('Bid too low, sorry!');
-    //     return startAuction();
-    // }
-    // // if new high bidder, update item with new value based on its id
-    // const updateBidRes = await updateBid(id, parseFloat(bid_amount).toFixed(2));
-    // console.log(updateBidRes.message);
-
-    return startAuction();
+    return startApp();
 };
 
-// // function to delete an employee
-// const updateEmployee = async () => {
-//     // get all auction items so user can see what's there
-//     const items = await getAllItems();
+// // function to update an employee
+const updateEmp = async () => {
 
-//     // print all of the items
-//     console.table(items);
+    // get all employees so user can see what's there
+    //console.log("inside getallemps");
+    const employees = await getAllEmployees();
 
-//     // enter bid information and get id and amount back
-//     const { item_id, bid_amount } = await inquirer.prompt(bidQuestions);
+    // print all of the items
+    console.table(employees);
 
-//     // get item we want to bid on based on id
-//     const { id, highest_bid } = items.find(({ id }) => id === parseInt(item_id));
+    // enter employee id to be deleted
+    const { id, updateAction, new_id } = await inquirer.prompt(updateEmployeeQuestions);
 
-//     // if the current high bid is higher than the users, restart auction
-//     if (highest_bid > bid_amount) {
-//         console.log('Bid too low, sorry!');
-//         return startAuction();
-//     }
-//     // if new high bidder, update item with new value based on its id
-//     const updateBidRes = await updateBid(id, parseFloat(bid_amount).toFixed(2));
-//     console.log(updateBidRes.message);
+    // depending on the answer, do an action
+    //choices: [{ name: 'Employee Role ID' }, { name: 'Employee Manager ID' }, { name: 'Exit' }]
+    if (updateAction === 'Employee Role ID') {
+        const updateEmpRole = await updateEmployeeRole(id, new_id);
+        console.log(updateEmpRole.message);
+    } else if (updateAction === 'Employee Manager ID') {
+        const updateEmpManager = await updateEmployeeManager(id, new_id);
+        console.log(updateEmpManager.message);
+    } else if (updateAction === 'Exit') {
+        return startApp();
+    }
 
-//     return startAuction();
-// };
+    return startApp();
+};
 
 // connect to the db and start up auction
 connection.connect(err => {
